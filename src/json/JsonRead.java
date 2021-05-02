3785f6364d88a1b386888a9290a5e6644445543f
Praneeth <18praneeth@gmail.com>
2021-05-02 15:55:55 +0530

    Read File from URL

--- src/json/JsonRead.java
+++ src/json/JsonRead.java
@@ -5,24 +5,25 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 
 public class JsonRead {
     public static void main(String[] args) throws IOException, ParseException {
         JSONParser jsonparser = new JSONParser();
-        FileReader reader = new FileReader(".\\src\\json\\data.json");
-
+        URL covid = new URL("https://api.covid19india.org/data.json");
+        URLConnection cc = covid.openConnection();
+        BufferedReader reader = new BufferedReader((new InputStreamReader(cc.getInputStream())));
         Object obj = jsonparser.parse(reader);
         JSONObject covid_json = (JSONObject)obj;
 
-        String s_name = (String) covid_json.get("name");
-        JSONArray json_array = (JSONArray) covid_json.get("subject");
+        JSONArray state_wise_array = (JSONArray) covid_json.get("statewise");
+        System.out.println(state_wise_array.size());
+        for (Object o : state_wise_array) {
 
-        System.out.println(s_name);
-        for (Object o : json_array) {
-            String course = (String) o;
-            System.out.println(course);
+            System.out.println(o);
         }
 
     }
