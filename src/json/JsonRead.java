cca17285569a068defa20f2c1775586e0269b945
Priya Nayak <priyanayak.cs19@rvce.edu.in>
2021-05-09 13:33:54 +0530

    yayyy!!Graaphhhh cameeee :)

--- src/json/JsonRead.java
+++ src/json/JsonRead.java
@@ -1,7 +1,14 @@
 package json;
-
+//package application;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.application.Application;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import javafx.scene.layout.StackPane;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.*;
@@ -9,8 +16,63 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.net.URLConnection;
 import java.util.Date;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+public class JsonRead extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        JSONParser jsonparser = new JSONParser();
+        URL covid = new URL("https://api.covid19india.org/data.json");
+        URLConnection cc = covid.openConnection();
+        BufferedReader reader = new BufferedReader((new InputStreamReader(cc.getInputStream())));
+        Object obj = jsonparser.parse(reader);
+        JSONObject covid_json = (JSONObject)obj;
+        JSONArray daily_cases_array=(JSONArray) covid_json.get("cases_time_series");
+        NumberAxis xaxis = new NumberAxis(1, 30, 1);
+        NumberAxis yaxis = new NumberAxis(0, 200000, 5);
 
-public class JsonRead {
+        xaxis.setLabel("Date");
+        yaxis.setLabel("Number of cases");
+
+
+        LineChart linechart = new LineChart(xaxis, yaxis);
+
+
+        XYChart.Series series = new XYChart.Series();
+
+
+        SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+        Date d1 = sdformat.parse("2021-04-01");
+        int i = 1;
+        for (Object o :daily_cases_array) {
+            JSONObject daily = (JSONObject) o;
+            Date d2 = sdformat.parse((String) daily.get("dateymd"));
+            //  System.out.println("Date" + d2);
+            if (d2.compareTo(d1) >= 0) {
+                // JSONObject daily = (JSONObject)o;
+                series.getData().add(new XYChart.Data(i, daily.get("totaldeceased")));
+                i++;
+            }
+            series.setName("Number of people infected in april");
+
+            //adding series to the linechart
+            linechart.getData().add(series);
+
+            //setting Group and Scene
+            Group root = new Group(linechart);
+            root.getChildren().add(linechart);
+            Scene scene = new Scene(root, 600, 400);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Number of people infected in april");
+            primaryStage.show();
+        }
+    }
+
 
     static void printStateData(JSONArray obj) {
         for (Object o : obj) {
@@ -22,22 +84,23 @@ public class JsonRead {
             System.out.println("=================");
         }
     }
-        static void printDailyData(JSONArray obj1) throws java.text.ParseException {
-            SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
-            Date d1 = sdformat.parse("2021-04-01");
-            for(Object o :obj1){
-                JSONObject daily = (JSONObject)o;
-                Date d2= sdformat.parse((String) daily.get("dateymd")) ;
-              //  System.out.println("Date" + d2);
-               if(d2.compareTo(d1)>=0) {
-                   // JSONObject daily = (JSONObject)o;
-                   System.out.println("Date" + daily.get("date"));
-                   System.out.println("Number of deceased" + daily.get("totaldeceased"));
 
-               }
+    static void printDailyData(JSONArray obj1) throws java.text.ParseException {
+        SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+        Date d1 = sdformat.parse("2021-04-01");
+        for (Object o : obj1) {
+            JSONObject daily = (JSONObject) o;
+            Date d2 = sdformat.parse((String) daily.get("dateymd"));
+            //  System.out.println("Date" + d2);
+            if (d2.compareTo(d1) >= 0) {
+                // JSONObject daily = (JSONObject)o;
+                System.out.println("Date" + daily.get("date"));
+                System.out.println("Number of deceased" + daily.get("totaldeceased"));
+
             }
         }
-        static void printVaccineData(JSONArray obj1) throws java.text.ParseException
+    }
+       /* static void printVaccineData(JSONArray obj1) throws java.text.ParseException
         {
             SimpleDateFormat sdformat = new SimpleDateFormat("dd/MM/yyyy");
             Date d1 = sdformat.parse("17/01/2021");
@@ -54,7 +117,10 @@ public class JsonRead {
                     System.out.println("Date" + daily.get("date"));
                     System.out.println("Number of people vaccinated" + daily.get("totalindividualsvaccinated"));
                 }
-            }}
+            }}*/
+
+
+
 
     public static void main(String[] args) throws IOException, ParseException, java.text.ParseException {
         JSONParser jsonparser = new JSONParser();
@@ -66,12 +132,13 @@ public class JsonRead {
 
         JSONArray state_wise_array = (JSONArray) covid_json.get("statewise");
         JSONArray daily_cases_array=(JSONArray) covid_json.get("cases_time_series");
-        JSONArray vaccine_cases_array=(JSONArray) covid_json.get("tested");
+       // JSONArray vaccine_cases_array=(JSONArray) covid_json.get("tested");
             System.out.println(state_wise_array.size());
         printStateData(state_wise_array);
         System.out.println("====================================================");
         printDailyData(daily_cases_array);
-        printVaccineData(vaccine_cases_array);
+      //  printVaccineData(vaccine_cases_array);
+        launch(args);
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,38 +1,87 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
 
 
-public class Main extends Application {
+public class Main extends Application{
+    static void Getcoviddata(XYChart.Series series) throws IOException, ParseException, java.text.ParseException {
 
-     @Override
-     public void start(Stage primaryStage) throws Exception {
-         // TODO Auto-generated method stub
-         Button btn1=new Button("Say, Hello World");
-         btn1.setOnAction(new EventHandler<ActionEvent>() {
+        JSONParser jsonparser = new JSONParser();
+        URL covid = new URL("https://api.covid19india.org/data.json");
+        URLConnection cc = covid.openConnection();
+        BufferedReader reader = new BufferedReader((new InputStreamReader(cc.getInputStream())));
+        Object obj = jsonparser.parse(reader);
+        JSONObject covid_json = (JSONObject) obj;
+        JSONArray daily_cases_array = (JSONArray) covid_json.get("cases_time_series");
+        int i = 1;
+        SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+        Date d1 = sdformat.parse("2021-04-01");
+        for (Object o : daily_cases_array) {
+            JSONObject daily = (JSONObject) o;
+            Date d2 = sdformat.parse((String) daily.get("dateymd"));
+            //  System.out.println("Date" + d2);
+            if (d2.compareTo(d1) >= 0) {
 
-             @Override
-             public void handle(ActionEvent arg0) {
-                 System.out.println("hello world");
-             }
-         });
-         StackPane root=new StackPane();
-         root.getChildren().add(btn1);
-         Scene scene=new Scene(root,600,400);
-         primaryStage.setTitle("First JavaFX Application");
-         primaryStage.setScene(scene);
-         primaryStage.show();
-     }
 
-     public static void main(String[] args) {
+                // JSONObject daily = (JSONObject)o;
+                series.getData().add(new XYChart.Data(i, Integer.parseInt((String) daily.get("totaldeceased"))));
+                i++;
+            }
+        }
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        NumberAxis xAxis = new NumberAxis(1, 30, 1);
+        xAxis.setLabel("Dates");
+        NumberAxis yAxis = new NumberAxis(0, 400000, 1000);
+        yAxis.setLabel("Number of people infected");
+
+        LineChart linechart = new LineChart(xAxis, yAxis);
+        XYChart.Series series = new XYChart.Series();
+
+        Getcoviddata(series);
+
+        series.setName("Number of people infected in the month of april");
+        linechart.getData().add(series);
+        Group root = new Group();
+        root.getChildren().add(linechart);
+        Scene scene = new Scene(root, 700, 700);
+        primaryStage.setTitle("Number of people infected in the month of april");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main (String[] args)
+    {
         launch(args);
     }
+
 }
